5fdfe4444ea6e729dd5f99aa26bd63f0f98e13df
Ryan Malonzo <64932650+ryanmalonzo@users.noreply.github.com>
2025-11-29 23:09:18 +0100

    jellyfin

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -55,10 +55,28 @@
           userns = "keep-id";
         };
       };
+
+      jellyfin = {
+        autoStart = true;
+        serviceConfig = {
+          RestartSec = "10";
+          Restart = "always";
+        };
+        containerConfig = {
+          image = "docker.io/jellyfin/jellyfin:latest";
+          publishPorts = [ "8096:8096" ];
+          userns = "keep-id";
+          volumes = [
+            "jellyfin-config:/config:Z"
+            "jellyfin-cache:/cache:Z"
+            "jellyfin-media:/media:Z"
+          ];
+        };
+      };
     };
   };
 
-  networking.firewall.allowedTCPPorts = [ 8080 ];
+  networking.firewall.allowedTCPPorts = [ 8080 8096 ];
 
   environment.systemPackages = with pkgs; [
     git
